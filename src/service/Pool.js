b9557b771d8560087d33c7d556b50256ddce951e
nahuel casagrande <34272854+cvpanda@users.noreply.github.com>
2023-10-30 14:47:09 -0300

    Update Pool.js

--- src/service/Pool.js
+++ src/service/Pool.js
@@ -1,11 +1,8 @@
 import { CognitoUserPool } from 'amazon-cognito-identity-js'
 
-//DESCOMENTAR
-//COMENTAR
-
 const poolData = {
-  UserPoolId: 'sa-east-1_sBWQgMjst', //prod'sa-east-1_sIILFybeB', //qa 'sa-east-1_sBWQgMjst',// us-east-1_ti4m5Dzd7
-  ClientId: '30iggs2dg9hui9j411soijbkmi' //prod'3t7sl05m4b2iacljjnc1nts3um', //qa '30iggs2dg9hui9j411soijbkmi' // 5djkmkfpce2j01g83rthvuj8ca
+  UserPoolId: 'sa-east-1_fake',
+  ClientId: '12345fake'
 }
 
 const UserPool = new CognitoUserPool(poolData)
